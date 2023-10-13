b79b1ceeceb6d57ab7c35e0294858e7b4d222f39
Steven Nunez <octosteve@github.com>
2023-10-13 15:58:31 -0400

    Create output dir

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 root="/opt/test-runner"
 export PYTHONPATH="$root:$PYTHONPATH"
-# run bin/run.py from action root
+mkdir autograding_output
+
 python3 /opt/test-runner/bin/run.py octosteve-sample-tests ./ ./autograding_output/
 
 echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/result.json)" >> $GITHUB_OUTPUT
