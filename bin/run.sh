2421c6920a817f9d593c6271dd161910c87a34a2
Steven Nunez <octosteve@github.com>
2023-10-13 16:22:30 -0400

    Debug

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+
+set -ex
 root="/opt/test-runner"
 export PYTHONPATH="$root:$PYTHONPATH"
 mkdir autograding_output
 
 python3 /opt/test-runner/bin/run.py ./ ./autograding_output/
-
+echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/results.json | jq -c .)" 
 echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/results.json | jq -c .)" >> $GITHUB_OUTPUT
