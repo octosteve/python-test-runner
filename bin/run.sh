f07b92dce274ddad69453795285bad0841997f92
Steven Nunez <octosteve@github.com>
2023-10-13 17:13:48 -0400

    Remove debug

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-set -ex
 root="/opt/test-runner"
 export PYTHONPATH="$root:$PYTHONPATH"
+
 mkdir autograding_output
 
 python3 /opt/test-runner/bin/run.py ./ ./autograding_output/
-echo "result=$(jq -c . results.json | jq -sRr @base64)"
+
 echo "result=$(jq -c . autograding_output/results.json | jq -sRr @base64)" >> $GITHUB_OUTPUT
