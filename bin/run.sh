bcf1a92f0df3fd97a8be901ad30ac38cdeedcece
Steven Nunez <octosteve@github.com>
2023-10-13 16:28:18 -0400

    URI encode output

--- bin/run.sh
+++ bin/run.sh
@@ -6,5 +6,5 @@ export PYTHONPATH="$root:$PYTHONPATH"
 mkdir autograding_output
 
 python3 /opt/test-runner/bin/run.py ./ ./autograding_output/
-echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/results.json | jq -c .)" 
-echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/results.json | jq -c .)" >> $GITHUB_OUTPUT
+echo "result=$(jq -c . results.json | jq -sRr @uri)"
+echo "result=$(jq -c . results.json | jq -sRr @uri)" >> $GITHUB_OUTPUT
