644a6622dc3b2a96c584a27d5aa52c32bc4122b8
Steven Nunez <octosteve@github.com>
2023-10-13 15:56:59 -0400

    slugs

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,6 @@
 root="/opt/test-runner"
 export PYTHONPATH="$root:$PYTHONPATH"
 # run bin/run.py from action root
-python3 /opt/test-runner/bin/run.py octosteve_sample_tests ./ ./autograding_output/
+python3 /opt/test-runner/bin/run.py octosteve-sample-tests ./ ./autograding_output/
 
 echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/result.json)" >> $GITHUB_OUTPUT
