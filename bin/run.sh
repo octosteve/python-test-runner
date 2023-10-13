29b3706a8c5197177039b05d793568bd3768c0f0
Steven Nunez <octosteve@github.com>
2023-10-13 15:45:27 -0400

    Try action

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 root="$( dirname "$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )" )"
 export PYTHONPATH="$root:$PYTHONPATH"
-python3 bin/run.py "$@"
+python3 bin/run.py octosteve_sample_tests $GITHUB_WORKSPACE/ $GITHUB_WORKSPACE/autograding_output/
+
+echo "result=$(cat $GITHUB_WORKSPACE/autograding_output/result.json | jq -c .)" >> $GITHUB_OUTPUT
